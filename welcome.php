9a1837b58a47c63ef9a358c4ce40abdd7dd25503
Meriel Necole T. Manuel <56289574+mnmerielmanuel@users.noreply.github.com>
2021-03-05 22:37:21 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -14,7 +14,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Welcome</title>
+        <title>WELCOME</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
     </head>
